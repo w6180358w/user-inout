62093c217c19d857e51b94e67dde18a32520a1a5
w6180358w <272416634@163.com>
2022-10-23 20:38:02 +0800

    .

--- src/main/java/com/search/search/SearchApplication.java
+++ src/main/java/com/search/search/SearchApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SearchApplication.class, args);
+		SpringApplication.run(SearchApplication.class, args); 
 	}
 
 }
